7731a49a2607197764d55b3ca633a3dc8e9eb8ae
Dev Shah <djshah2903@gmail.com>
2026-08-22 20:02:57 -0400

    UI Elements for Rock Paper Scissors via DOM manipulation

--- html-boilerplate/rps.js
+++ html-boilerplate/rps.js
@@ -4,6 +4,9 @@ console.log("Hello, World!")
 const options = ['rock', 'paper', 'scissors'];
 let humanScore = 0;
 let computerScore = 0;
+let gameOver = false
+let message = "";
+let count = 1
 
 const winningRules = {
     rock: 'scissors',
@@ -28,6 +31,15 @@ options.forEach((option) => {
     div.appendChild(button)
 })
 
+const pElement = document.createElement('p')
+const pScore = document.createElement('p')
+const newGameButton = document.createElement('button')
+div.appendChild(pElement)
+div.appendChild(pScore)
+div.appendChild(newGameButton)
+newGameButton.textContent = "New Game"
+newGameButton.style.display = "none";
+
 
 
 // FUNCTIONS
@@ -47,29 +59,64 @@ function compare(humanChoice, computerChoice) {
     }
 }
 
-function logResult(message){
-    const p = document.createElement('p')
-    p.textContent = message
-    div.appendChild(p)
+
+function updateDisplay(){
+    pElement.textContent = message;
+    pScore.textContent = `YOU: ${humanScore} — COMPUTER: ${computerScore}`;
 }
 
-function playRound(humanChoice, computerChoice) {
-    const winner = compare(humanChoice, computerChoice)
-
-    logResult(computerChoice)
-    logResult(humanChoice)
+function scoreTracking(winner){
     if (winner === 'human') {
         humanScore = humanScore + 1;
-        logResult("You win! " + humanChoice + " beats " + computerChoice)
     } else if (winner === 'computer') {
         computerScore = computerScore + 1;
-        logResult("You lose! " + computerChoice + " beats " + humanChoice)
-    } else {
-        logResult("It's a tie!")
+    } 
+}
+
+function describeRound(humanChoice, computerChoice){
+    if (winningRules[humanChoice] === computerChoice){
+        return `${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} > ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`
+    } else if (humanChoice === computerChoice){
+        return `TIE!`
+    } else{
+        return `${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} > ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`
     }
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    if (gameOver) return;
+    const winner = compare(humanChoice, computerChoice)
+    scoreTracking(winner)
+    
+    message = `Round ${count}: ${describeRound(humanChoice, computerChoice)}`;
+    count += 1;
+    
+    if (humanScore >= 5) {
+        message = "GAME OVER! YOU WON!"
+        gameOver = true
+        newGameButton.style.display = ""
+    } else if (computerScore >= 5) {
+        message = "GAME OVER! YOU LOST!"
+        gameOver = true
+        newGameButton.style.display = ""
+    }
+    updateDisplay()
 
 }
 
+function resetGame(){
+    humanScore = 0
+    count = 1
+    computerScore = 0
+    gameOver = false
+    message = ""
+    newGameButton.style.display = "none"
+    updateDisplay()
+}
+
+newGameButton.addEventListener("click", resetGame)
+updateDisplay()
 // function playGame(){
 //     for (let i=1; i <= 5; i++){
 //         playRound(compare)
